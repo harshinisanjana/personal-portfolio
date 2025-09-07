569cbdb388165df3596c36aaf85228bf7428b8ca
Harshini Sanjana J <harshinisanjana.j@gmail.com>
2025-09-07 22:55:02 +0530

    meh

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home } from "./pages/Home";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/personal-portfolio/">
         <Routes>
           <Route index element={<Home />} />
         </Routes>
