25635ca2f52a11607bb60f67351e7cb149fed582
Harshini Sanjana J <harshinisanjana.j@gmail.com>
2025-09-07 22:15:25 +0530

    Removed toaster import and usage

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,9 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : process.env.VITE_BASE_PATH || "/personal-portfolio",
+  base : process.env.VITE_BASE_PATH || "/personal-portfolio",  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
