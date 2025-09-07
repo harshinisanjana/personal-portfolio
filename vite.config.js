1757988e9f28277cae665b419f790a00aa9aad06
Harshini Sanjana J <harshinisanjana.j@gmail.com>
2025-09-07 22:19:23 +0530

    Fix Vercel 404 issue

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : process.env.VITE_BASE_PATH || "/personal-portfolio",  resolve: {
+  base: "./",  
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
+    },
+  },
 })
