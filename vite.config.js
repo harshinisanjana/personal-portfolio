9699d570700aa5ed0d9d1f088a7ddf85f869fcb9
Harshini Sanjana J <harshinisanjana.j@gmail.com>
2025-09-08 19:42:47 +0530

    Fix project image paths in ProjectsSection

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/',  
+  base: '/personal-portfolio/',  
 })
